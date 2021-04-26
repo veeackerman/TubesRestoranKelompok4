3ede10339a252d55ddd327b03f92eccfb0661fa5
veeackerman <82373301+veeackerman@users.noreply.github.com>
2021-04-26 11:48:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
   require_once 'includes/header.php';
 ?>
 
-  <br>
+  <div class="p-3 mb-2 bg-dark text-white">
     <h1 class=text-center >Table Reservation </h1>
+  </div>
   <br>
   <br>
   <br>
@@ -12,26 +13,31 @@
     <div class="container">
   <div class="row">
     <div class="col-sm">
+        </div>
+      <div class="col-sm">
+      <div class="p-3 mb-2 bg-dark text-white">
+          <div class="login_form">
+          <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Alamat email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Ingat saya</label>
+        </div>
+        <div class="d-grid gap-2 col-6 mx-auto">
+        <button type="submit" class="btn btn-warning">Masuk</button>
+        <button type="signup" class="btn btn-outline-warning">Daftar</button>
+        </div>
+      </form>
       </div>
-    <div class="col-sm">
-        <div class="login_form">
-        <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Alamat email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Ingat saya</label>
-      </div>
-      <button type="submit" class="btn btn-dark align-middle">Masuk</button>
-    </form>
-    </div>
-    </div>
     <div class="col-sm">
       </div>
   </div>
