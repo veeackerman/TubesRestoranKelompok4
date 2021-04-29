45c2c7a7433f98e999bd7911b915276a4637cefc
veeackerman <82373301+veeackerman@users.noreply.github.com>
2021-04-29 09:38:31 +0800

    Add files via upload

---
+++ terdaftar.php
@@ -0,0 +1,11 @@
+<?php 
+  $title = 'Table Reservation';
+  require_once 'includes/header.php';
+?>
+
+<h2>Berhasil!</h2>
+<form method="get" action="index.php">
+<button type="signup" class="btn btn-link mb-5 p-3">Masuk</button>
+</form>
+
+<?php require_once 'includes/footer.php';?>
