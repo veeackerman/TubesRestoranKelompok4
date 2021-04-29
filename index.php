29fb5f09362aaef489497c4a0a41febe8e935937
veeackerman <82373301+veeackerman@users.noreply.github.com>
2021-04-29 12:12:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,35 +10,41 @@
   <br>
 
     <div class="container">
-  <div class="row">
-    <div class="col-sm">
-        </div>
+    <div class="row">
       <div class="col-sm">
-      <div class="p-4 mb-2 bg-dark text-white gradient bayangan rcorners1">
-          <form method="get" action="success.php">
-          <div class="login_form">
-          <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Alamat email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+        <div class="col-sm">
+          <div class="p-4 mb-2 bg-dark text-white gradient bayangan rcorners1">
+              <form method="post" action="success.php">
+              <div class="login_form">
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Alamat email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="text-white">
+              <?php
+               if(isset($_COOKIE["message"])){ // Jika ada
+                echo $_COOKIE["message"]; // Tampilkan pesannya
+                }
+              ?>
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Ingat saya</label>
+            </div>
+            <div class="mb-1 d-grid gap-2 col-4 mx-auto">
+            <button id="masuk" type="submit" class="btn btn-warning">Masuk</button>
+            </div>
+          </form>
+          </div>
+          </div>
+          </div>
+      <div class="col-sm">
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Ingat saya</label>
-        </div>
-        <div class="mb-1 d-grid gap-2 col-4 mx-auto">
-        <button type="submit" class="btn btn-warning">Masuk</button>
-        </div>
-      </form>
-      </div>
-      </div>
-      </div>
-    <div class="col-sm">
-      </div>
   </div>
 <form method="get" action="daftar.php">
 </div>
